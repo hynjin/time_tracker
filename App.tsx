ef6e0b967c87587baafdbcd8f23f19b9f859c8b1
hyunjin <hynjin0110@gmail.com>
2024-06-23 22:16:48 -0400

    remove stopwatch library

--- App.tsx
+++ App.tsx
@@ -1,122 +1,7 @@
-// import React from "react";
-// import type { PropsWithChildren } from "react";
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from "react-native";
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from "react-native/Libraries/NewAppScreen";
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({ children, title }: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === "dark";
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}
-//       >
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}
-//       >
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === "dark";
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? "light-content" : "dark-content"}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}
-//       >
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}
-//         >
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to sesdfsfse your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discsdfover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: "600",
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: "400",
-//   },
-//   highlight: {
-//     fontWeight: "700",
-//   },
-// });
-
-// export default App;
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BlocksScreen from "./components/BlocksScreen";
-import StopWatchScreen from "./components/StopWatchScreen";
+// import StopWatchScreen from "./components/StopWatchScreen";
 import AnalyticsScreen from "./AnalyticsScreen";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
@@ -126,14 +11,14 @@ function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName="StopWatch"
+        initialRouteName="Blocks"
         screenOptions={{
           // tabBarStyle: { display: "none" },
           swipeEnabled: true,
         }}
       >
         <Tab.Screen name="Blocks" component={BlocksScreen} />
-        <Tab.Screen name="StopWatch" component={StopWatchScreen} />
+        {/* <Tab.Screen name="StopWatch" component={StopWatchScreen} /> */}
         <Tab.Screen name="Analytics" component={AnalyticsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
